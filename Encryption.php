9e9c738db72c7c3afa97747cf6302357c78153c3
Francesco Cartenì <francesco.carteni@gmail.com>
2017-05-16 18:19:30 +0200

    - Switch to 2.0.0 - Deprecations removed. - Added methods for support to resource encryption. - Added new tests. - Updated docs.

--- Encryption.php
+++ Encryption.php
@@ -16,6 +16,9 @@ use Defuse\Crypto\File as BaseCryptoFile;
 use Mes\Security\CryptoBundle\Model\Key;
 use Mes\Security\CryptoBundle\Model\KeyInterface;
 
+/**
+ * Class Encryption.
+ */
 final class Encryption extends AbstractEncryption
 {
     /**
